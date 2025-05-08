2d926c9a836198964df05f006b809d8ae59ba6bf
susumOyaji <sumitomo0210@gmail.com>
2025-05-08 11:28:37 +0900

    upusa

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub struct Article {
 #[wasm_bindgen]
 pub async fn fetch_movie_keywords() -> Result<JsValue, JsValue> {
     //let url = "https://yoshizo.hatenablog.com/entry/microsoft-rewards-search-keyword-list/#movie";
-    let url = "https://jp.investing.com/equities/sony-corp.";
+    let url = "https://www.yahoo.com/?guccounter=1";
     
     let opts = RequestInit::new();
     opts.set_method("GET");
