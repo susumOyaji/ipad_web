2d38b6c277199d0a5e66304f57f864ce7db6af7a
susumOyaji <sumitomo0210@gmail.com>
2025-05-08 09:58:18 +0900

    up

---
+++ pkg/ipad_web_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const fetch_movie_keywords: () => any;
+export const main: () => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_5: WebAssembly.Table;
+export const closure38_externref_shim: (a: number, b: number, c: any) => void;
+export const closure190_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
